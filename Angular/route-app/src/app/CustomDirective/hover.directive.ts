cb008ccb23c8d9607c0c784393798d86bb01d1d4
rahulrobosoft <rahul.robosoftin.com>
2022-10-11 15:33:24 +0530

    added hostBinding sample

--- Angular/route-app/src/app/CustomDirective/hover.directive.ts
+++ Angular/route-app/src/app/CustomDirective/hover.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHover]'
@@ -7,13 +7,21 @@ export class HoverDirective {
 
   constructor(private element:ElementRef, private renderer: Renderer2) { }
 
+  @HostBinding('style.backgroundColor') bg:string = '#5D7E9D';
+  @HostBinding('style.transition') transition:string = '';
+
+
   @HostListener('mouseenter') onMouseEnter (){
-    this.renderer.setStyle(this.element.nativeElement,'backgroundColor','#194775')
+    // this.renderer.setStyle(this.element.nativeElement,'backgroundColor','#194775')
+    this.bg = '#194775'
   }
 
   @HostListener('mouseleave') onMouseLeave(){
-    this.renderer.setStyle(this.element.nativeElement,'backgroundColor','#5D7E9D')
-    this.renderer.setStyle(this.element.nativeElement,'transition','0.5s')
+    // this.renderer.setStyle(this.element.nativeElement,'backgroundColor','#5D7E9D')
+    // this.renderer.setStyle(this.element.nativeElement,'transition','0.5s')
+
+    this.bg = '#5D7E9D';
+    this.transition = '0.5s';
 
   }
 
