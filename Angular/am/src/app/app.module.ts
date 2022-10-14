6005c6aebb7d1f4746d526717cd39af4e1b26d23
rahulrobosoft <rahul.robosoftin.com>
2022-10-14 17:30:07 +0530

    Updates

--- Angular/am/src/app/app.module.ts
+++ Angular/am/src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
-
 
 @NgModule({
   declarations: [
@@ -15,10 +13,8 @@ import { MaterialModule } from './material/material.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
     BrowserAnimationsModule,
     MaterialModule,
-
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular/rf/src/app/app.component.ts
+++ Angular/rf/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup,Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,32 +8,45 @@ import { FormArray, FormControl, FormGroup,Validators } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   title = 'rf';
-  reactiveForm!:FormGroup;
-  count:number =0;
+  reactiveForm!: FormGroup;
+  count: number = 0;
+  dis:boolean=true;
 
-  ngOnInit(){
+  ngOnInit() {
     this.reactiveForm = new FormGroup({
-      name : new FormControl(null,[Validators.required,Validators.pattern('[a-zA-Z ]*')]),
-      age : new FormControl(null,[Validators.required,Validators.min(21),Validators.max(40)]),
-      email : new FormControl(null,[Validators.required,Validators.email]),
-      mobile : new FormControl(null,[Validators.required,Validators.minLength(10),Validators.maxLength(10)]),
-      gender : new FormControl(null,Validators.required),
+      name: new FormControl(null, [Validators.required, Validators.pattern('[a-zA-Z ]*')]),
+      age: new FormControl(null, [Validators.required, Validators.min(21), Validators.max(40)]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      mobile: new FormControl(null, [Validators.required, Validators.minLength(10), Validators.maxLength(10),Validators.pattern('[0-9]*')]),
+      gender: new FormControl(null, Validators.required),
 
-      qualities : new FormArray([
-        //  new FormControl(''),
-        new FormControl(null,Validators.required),
+      qualities: new FormArray([
+        new FormControl(null, Validators.required),
       ]),
-      
+
+      details:new FormArray([
+        new FormControl(null),
+      ])
+
     })
   }
 
-  onSubmit(){
+  onSubmit() {
     console.log(this.reactiveForm);
+    // console.log(this.reactiveForm.qualities);
+    this.reactiveForm.reset();
+    console.log(<FormArray>this.reactiveForm.get('age').value);
+    alert('Thank you! You will find your match soon');
+
   }
 
-  addQ(){
-  //  (<FormArray>this.reactiveForm.get('qualities')).push(new FormControl(null));
-   (<FormArray>this.reactiveForm.get('qualities')).push(new FormControl(null));
-  //  this.count++;
+  addQ() {
+    (<FormArray>this.reactiveForm.get('qualities')).push(new FormControl(null));
+    // this.count++;
+  }
+
+  addF(){
+    (<FormArray>this.reactiveForm.get('details')).push(new FormControl(null));
+    // (<FormArray>this.reactiveForm.get('details')).push(new FormControl(null));
   }
 }
